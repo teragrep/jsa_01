d9ebd03fdc711a26f5f5f24e0ffb36f95ff85740
Mayura Selvarajah <mayura.selvarajah@fail-safe.net>
2023-01-23 14:14:18 +0200

    Update the doc

--- src/test/js/test-index.js
+++ src/test/js/test-index.js
@@ -9,23 +9,10 @@ log4js.configure( {
   },
   categories: {
     jsa: { appenders: ['jsa'], level: 'error' },
-    another: { appenders: ['console'], level: 'trace' },
     default: { appenders: ['jsa'], level: 'trace' },
   },
 })
 
 const logger = log4js.getLogger('jsa');
-
-  // these will not appear (logging level beneath error)
-  logger.trace('Entering trace: First, solve the problem. Then, write the code');
-  logger.debug('Got TG.');
-  logger.trace('Entering trace: First, solve the problem. Then, write the code');
-  
-  logger.log('Something funny about DEBUG.');
-  logger.warn('BUG is quite smelly.');
-  // these end up 
-  //logger.error('FIX %s is over enginnered 😎!', 'Cheers!');
-  //logger.error('Entering trace: First, solve the problem. Then, write the code');
-  //logger.fatal('Don’t test it as a NASA application');  // ⛔ The Unicode character ' (U+2019 RIGHT SINGLE QUOTATION MARK) . So when pushing the log message with U+2019 fails but WHY???
-  logger.fatal("Don't  test it as a NASA application😎"); 
+logger.fatal("Don't  test it as a NASA application😎"); 
  
